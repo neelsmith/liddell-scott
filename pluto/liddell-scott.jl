fe475c6fe7b687b06e612968bd74edb608af5bc1
Neel Smith <dnsmith.neel@gmail.com>
2024-08-27 10:14:53 -0400

    Add Pluto nb

---
+++ pluto/liddell-scott.jl
@@ -0,0 +1,623 @@
+### A Pluto.jl notebook ###
+# v0.19.46
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ 5bdbfc84-62d5-11ef-2bdc-e7186887eee4
+begin
+	using Downloads
+	using Markdown
+	using PlutoUI
+	using PlutoTeachingTools
+	nbversion = "1.1.0"
+	md"""*Notebook version*: **$(nbversion)**. *See version info*: $(@bind versioninfo CheckBox())"""
+end
+
+# ╔═╡ 49e06755-2873-4db0-9326-83f07252bf7a
+if versioninfo
+md"""
+
+- **1.1.0**: adds option to limit search by lemma to initial string match
+- **1.0.0**: initial release
+"""
+end
+
+# ╔═╡ c9571b9f-8fd7-4145-a517-f562077b5292
+md"""*Use online copy of dictionary*: $(@bind remote CheckBox(true))"""
+
+# ╔═╡ 8befdede-ffe8-47c4-8676-3a8b1555c795
+md""" # Liddell-Scott *Greek-English Lexicon*"""
+
+# ╔═╡ 7fcc2f71-2a60-4e46-a622-5d165e5a1732
+md"""*Contents to search*: $(@bind searchby Select(["lemma" => "Lemma", "id" => "Article ID", "fulltext" => "Full text"]))"""
+
+# ╔═╡ b0fb6dad-ad4a-4b66-957f-d2cee7bbaaff
+if searchby == "lemma"
+	md"""*Limit to matching beginning of lemma* $(@bind initial CheckBox())"""
+else 
+	initial = false
+	nothing
+end
+
+# ╔═╡ 242b68cf-2ca1-4901-9049-07f23c2eabdd
+TableOfContents()
+
+# ╔═╡ fb5cf0cf-edc1-4bda-bd35-9d45ea1bf746
+html"""
+<br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/>
+<br/><br/><br/><br/><br/>
+<hr/>
+"""
+
+# ╔═╡ 153c5459-62db-46df-8bc2-d258f65adaf6
+md"""> **About**"""
+
+# ╔═╡ 2d66a73c-8535-4023-a6a7-0d96cc2e78ec
+about = """
+
+This notebook searches articles extracted from a digital edition of Liddell and Scott's *Greek Lexicon* in Markdown formatting by Christopher Blackwell, and [freely available on github](https://github.com/Eumaeus/cite_lsj_cex).
+
+The articles are in a simple delimited-text format with a sequence number, an identifying CITE2 URN, a lemma string, and the full article.  You can download the extracted articles [here](http://shot.holycross.edu/lexica/ls-articles.cex).
+
+"""
+
+# ╔═╡ c4d5efa7-0dc2-4a54-ac07-b71392d8360f
+Foldable("About the markdown lexicon",Markdown.parse(about))
+
+# ╔═╡ e3d712e2-cd3d-41fe-90c5-bbdc32cc7e28
+md""" > **Mechanics**"""
+
+# ╔═╡ 90d514a7-47e3-4c63-9cde-b73fedd44571
+f = "lsj-articles.cex"
+
+# ╔═╡ b28c4c40-c155-4607-b3bd-da1392344727
+lemmfile = "lsj-lemmas-alphabetic.txt"
+
+# ╔═╡ 5ff94e1f-312c-49ea-b58c-bd85a6cdf69b
+"""Read Liddell-Scott articles."""
+function read_lsj(f = "lsj-articles.cex"; remote = true)
+    if remote
+        url = "http://shot.holycross.edu/lexica/lsj-articles.cex"
+        f = Downloads.download(url)
+        content = readlines(f)
+        rm(f)
+        content
+
+    else
+        readlines(f)
+    end
+end
+
+# ╔═╡ 9059bd6e-3e74-4afd-8817-3511862dabe1
+"""Read list of alphabetic-only lemmas to search."""
+function read_lemmas(f = "lsj-lemmas-alphabetic.txt"; remote = true)
+    if remote
+        url = "http://shot.holycross.edu/lexica/lsj-lemmas-alphabetic.txt"
+        f = Downloads.download(url)
+        content = readlines(f)
+        rm(f)
+        content
+
+    else
+        readlines(f)
+    end
+end
+
+# ╔═╡ 1d0ee8b4-ae47-45d3-9d15-f7c5d4504682
+articles = read_lsj(remote)
+
+# ╔═╡ 568c35b6-edb4-48dd-b808-08ead8a70127
+md"""*Search* **$(length(articles))** *articles in Liddell-Scott for*: $(@bind s confirm(TextField(default = "σχολιαζω")))"""
+
+# ╔═╡ edb0f620-a999-42ef-8e2a-9a5ffe185cc2
+lemmas = read_lemmas(remote)
+
+# ╔═╡ bd911a20-bae9-455c-a493-2295872185ae
+"""Format a list of data lines from delimited-text source as a single Markdown text.
+"""
+function format(entries)
+    formatted = map(entries) do entry
+        cols = split(entry,"|")
+        urn = cols[2]
+        lemma = cols[3]
+        text = cols[4]
+        string("## *", lemma, "*\n\n`", urn,"`\n\n", text)
+    end
+    join(formatted, "\n\n")
+end
+
+# ╔═╡ 6b59b1fe-06fd-496f-b2d7-2552866f26ad
+"""Sort articles following the Blackwell algorithm: exact lemma match comes out on top, then lemma substrings alphabetically sorted, then remaining articles in alphabetical order."""
+function blackwell_sort(matches, lemmastring)
+	lemmarestring = "([^\\|]+)\\|([^\\|]+)\\|$(lemmastring)(.*)"
+	lemmare = Regex(lemmarestring)
+	top = filter(s -> occursin(lemmare, s), matches)
+
+	lemmaanyrestring = "([^\\|]+)\\|([^\\|]+)\\|[^\\|]*$(lemmastring)[^\\|]*\\|(.*)"
+	lemma_any_re = Regex(lemmaanyrestring)
+	middle = filter(s -> occursin(lemma_any_re, s) && ! (occursin(lemmare, s)) , matches)
+	
+	bottom = filter(s -> (! occursin(lemma_any_re, s)) && (! (occursin(lemmare, s))) , matches)
+	
+	vcat(top, middle, bottom)
+
+end
+
+# ╔═╡ 01433609-9d68-4b04-a1e4-391d7c367d87
+"""Search a list of LSJ articles for identifying ID matching a string."""
+function id(s, articles)
+	pttrn = "urn:cite2:hmt:lsj.chicago_md:" * s * "\\|"
+    re = Regex(pttrn)
+    matches = filter(article -> occursin(re,article), articles)
+    formatted = blackwell_sort(matches, s) |> format
+    article = length(matches) == 1 ? "article" : "articles"
+    hdr = """# $(length(matches)) $(article) for ID *$(s)*\n\n""" 
+    string(hdr, formatted)
+end
+
+# ╔═╡ 5595aa1a-c807-4b8a-a64e-f7a81745a2f0
+"""Search a list of LSJ articles for lemma matching a string,
+optionally limiting search to matching the beginning of the lemma.
+"""
+function lemma(s; lemmas = lemmas, articles = articles, initial = false)
+    
+    pttrn =  initial ? "^$(s)" : s
+    re = Regex(pttrn)
+    match_indices = findall(lemm -> occursin(re, lemm), lemmas)
+    matches = map(i -> articles[i], match_indices)
+    formatted = blackwell_sort(matches, s) |> format
+    article = length(matches) == 1 ? "article" : "articles"
+
+	initlabel = initial ? " initial " : ""
+    hdr = """# $(length(matches)) $(article) with lemma matching $(initlabel) *$(s)*\n\n""" 
+    string(hdr, formatted)
+end
+
+# ╔═╡ fb0878b4-543f-488d-b62d-3d349904d301
+"""Search a list of articles for text matching a string."""
+function textsearch(s, articles = articles)
+    matches = filter(article -> occursin(s,article), articles)
+    formatted = blackwell_sort(matches, s) |> format
+    article = length(matches) == 1 ? "article" : "articles"
+    hdr = """# $(length(matches)) $(article) matching *$(s)*\n\n""" 
+    string(hdr, formatted)
+end
+
+# ╔═╡ f644526f-f62a-4a79-bced-ecbbd608205f
+"""Find articles containing string `s` and format
+for markdown display.
+"""
+function display(s; lemmas = lemmas, articles = articles, searchtype = "lemma", initial = false)
+	if searchtype == "id"
+		id(s, articles)
+		
+	elseif searchtype == "lemma"
+		lemma(s; lemmas = lemmas, articles = articles, initial = initial)
+		
+	else
+		textsearch(s, articles)
+	end
+	
+end
+
+
+
+# ╔═╡ 9c286167-2bf2-4685-ac63-bb0328a8f0cf
+begin
+	# Sleep just long enough for TableOfContents to catch up!
+	# see https://discourse.julialang.org/t/plutoui-jl-tableofcontents-update/100202
+	sleep(0.05)
+	display(s; searchtype = searchby, initial = initial) |> Markdown.parse
+end
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+Downloads = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+Markdown = "d6f4376e-aef5-505a-96c1-9c027394607a"
+PlutoTeachingTools = "661c6b06-c737-4d37-b85c-46df65de6f69"
+PlutoUI = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+
+[compat]
+PlutoTeachingTools = "~0.2.15"
+PlutoUI = "~0.7.59"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.10.1"
+manifest_format = "2.0"
+project_hash = "f245ed35a56b279f277dc3b5b7db1cec6b7d99be"
+
+[[deps.AbstractPlutoDingetjes]]
+deps = ["Pkg"]
+git-tree-sha1 = "6e1d2a35f2f90a4bc7c2ed98079b2ba09c35b83a"
+uuid = "6e696c72-6542-2067-7265-42206c756150"
+version = "1.3.2"
+
+[[deps.ArgTools]]
+uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
+version = "1.1.1"
+
+[[deps.Artifacts]]
+uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
+
+[[deps.Base64]]
+uuid = "2a0f44e3-6c83-55bd-87e4-b1978d98bd5f"
+
+[[deps.CodeTracking]]
+deps = ["InteractiveUtils", "UUIDs"]
+git-tree-sha1 = "7eee164f122511d3e4e1ebadb7956939ea7e1c77"
+uuid = "da1fd8a2-8d9e-5ec2-8556-3022fb5608a2"
+version = "1.3.6"
+
+[[deps.ColorTypes]]
+deps = ["FixedPointNumbers", "Random"]
+git-tree-sha1 = "b10d0b65641d57b8b4d5e234446582de5047050d"
+uuid = "3da002f7-5984-5a60-b8a6-cbb66c0b333f"
+version = "0.11.5"
+
+[[deps.CompilerSupportLibraries_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
+version = "1.1.0+0"
+
+[[deps.Dates]]
+deps = ["Printf"]
+uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
+
+[[deps.Distributed]]
+deps = ["Random", "Serialization", "Sockets"]
+uuid = "8ba89e20-285c-5b6f-9357-94700520ee1b"
+
+[[deps.Downloads]]
+deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
+uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
+version = "1.6.0"
+
+[[deps.FileWatching]]
+uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
+
+[[deps.FixedPointNumbers]]
+deps = ["Statistics"]
+git-tree-sha1 = "05882d6995ae5c12bb5f36dd2ed3f61c98cbb172"
+uuid = "53c48c17-4a7d-5ca2-90c5-79b7896eea93"
+version = "0.8.5"
+
+[[deps.Format]]
+git-tree-sha1 = "9c68794ef81b08086aeb32eeaf33531668d5f5fc"
+uuid = "1fa38f19-a742-5d3f-a2b9-30dd87b9d5f8"
+version = "1.3.7"
+
+[[deps.Hyperscript]]
+deps = ["Test"]
+git-tree-sha1 = "179267cfa5e712760cd43dcae385d7ea90cc25a4"
+uuid = "47d2ed2b-36de-50cf-bf87-49c2cf4b8b91"
+version = "0.0.5"
+
+[[deps.HypertextLiteral]]
+deps = ["Tricks"]
+git-tree-sha1 = "7134810b1afce04bbc1045ca1985fbe81ce17653"
+uuid = "ac1192a8-f4b3-4bfe-ba22-af5b92cd3ab2"
+version = "0.9.5"
+
+[[deps.IOCapture]]
+deps = ["Logging", "Random"]
+git-tree-sha1 = "b6d6bfdd7ce25b0f9b2f6b3dd56b2673a66c8770"
+uuid = "b5f81e59-6552-4d32-b1f0-c071b021bf89"
+version = "0.2.5"
+
+[[deps.InteractiveUtils]]
+deps = ["Markdown"]
+uuid = "b77e0a4c-d291-57a0-90e8-8db25a27a240"
+
+[[deps.JSON]]
+deps = ["Dates", "Mmap", "Parsers", "Unicode"]
+git-tree-sha1 = "31e996f0a15c7b280ba9f76636b3ff9e2ae58c9a"
+uuid = "682c06a0-de6a-54ab-a142-c8b1cf79cde6"
+version = "0.21.4"
+
+[[deps.JuliaInterpreter]]
+deps = ["CodeTracking", "InteractiveUtils", "Random", "UUIDs"]
+git-tree-sha1 = "7ae67d8567853d367e3463719356b8989e236069"
+uuid = "aa1ae85d-cabe-5617-a682-6adf51b2e16a"
+version = "0.9.34"
+
+[[deps.LaTeXStrings]]
+git-tree-sha1 = "50901ebc375ed41dbf8058da26f9de442febbbec"
+uuid = "b964fa9f-0449-5b57-a5c2-d3ea65f4040f"
+version = "1.3.1"
+
+[[deps.Latexify]]
+deps = ["Format", "InteractiveUtils", "LaTeXStrings", "MacroTools", "Markdown", "OrderedCollections", "Requires"]
+git-tree-sha1 = "ce5f5621cac23a86011836badfedf664a612cee4"
+uuid = "23fbe1c1-3f47-55db-b15f-69d7ec21a316"
+version = "0.16.5"
+
+    [deps.Latexify.extensions]
+    DataFramesExt = "DataFrames"
+    SparseArraysExt = "SparseArrays"
+    SymEngineExt = "SymEngine"
+
+    [deps.Latexify.weakdeps]
+    DataFrames = "a93c6f00-e57d-5684-b7b6-d8193f3e46c0"
+    SparseArrays = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+    SymEngine = "123dc426-2d89-5057-bbad-38513e3affd8"
+
+[[deps.LibCURL]]
+deps = ["LibCURL_jll", "MozillaCACerts_jll"]
+uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
+version = "0.6.4"
+
+[[deps.LibCURL_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
+uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
+version = "8.4.0+0"
+
+[[deps.LibGit2]]
+deps = ["Base64", "LibGit2_jll", "NetworkOptions", "Printf", "SHA"]
+uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
+
+[[deps.LibGit2_jll]]
+deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll"]
+uuid = "e37daf67-58a4-590a-8e99-b0245dd2ffc5"
+version = "1.6.4+0"
+
+[[deps.LibSSH2_jll]]
+deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
+uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
+version = "1.11.0+1"
+
+[[deps.Libdl]]
+uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
+
+[[deps.LinearAlgebra]]
+deps = ["Libdl", "OpenBLAS_jll", "libblastrampoline_jll"]
+uuid = "37e2e46d-f89d-539d-b4ee-838fcccc9c8e"
+
+[[deps.Logging]]
+uuid = "56ddb016-857b-54e1-b83d-db4d58db5568"
+
+[[deps.LoweredCodeUtils]]
+deps = ["JuliaInterpreter"]
+git-tree-sha1 = "1ce1834f9644a8f7c011eb0592b7fd6c42c90653"
+uuid = "6f1432cf-f94c-5a45-995e-cdbf5db27b0b"
+version = "3.0.1"
+
+[[deps.MIMEs]]
+git-tree-sha1 = "65f28ad4b594aebe22157d6fac869786a255b7eb"
+uuid = "6c6e2e6c-3030-632d-7369-2d6c69616d65"
+version = "0.1.4"
+
+[[deps.MacroTools]]
+deps = ["Markdown", "Random"]
+git-tree-sha1 = "2fa9ee3e63fd3a4f7a9a4f4744a52f4856de82df"
+uuid = "1914dd2f-81c6-5fcd-8719-6d5c9610ff09"
+version = "0.5.13"
+
+[[deps.Markdown]]
+deps = ["Base64"]
+uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
+
+[[deps.MbedTLS_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
+version = "2.28.2+1"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+
+[[deps.MozillaCACerts_jll]]
+uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
+version = "2023.1.10"
+
+[[deps.NetworkOptions]]
+uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
+version = "1.2.0"
+
+[[deps.OpenBLAS_jll]]
+deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
+uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
+version = "0.3.23+4"
+
+[[deps.OrderedCollections]]
+git-tree-sha1 = "dfdf5519f235516220579f949664f1bf44e741c5"
+uuid = "bac558e1-5e72-5ebc-8fee-abe8a469f55d"
+version = "1.6.3"
+
+[[deps.Parsers]]
+deps = ["Dates", "PrecompileTools", "UUIDs"]
+git-tree-sha1 = "8489905bcdbcfac64d1daa51ca07c0d8f0283821"
+uuid = "69de0a69-1ddd-5017-9359-2bf0b02dc9f0"
+version = "2.8.1"
+
+[[deps.Pkg]]
+deps = ["Artifacts", "Dates", "Downloads", "FileWatching", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
+uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
+version = "1.10.0"
+
+[[deps.PlutoHooks]]
+deps = ["InteractiveUtils", "Markdown", "UUIDs"]
+git-tree-sha1 = "072cdf20c9b0507fdd977d7d246d90030609674b"
+uuid = "0ff47ea0-7a50-410d-8455-4348d5de0774"
+version = "0.0.5"
+
+[[deps.PlutoLinks]]
+deps = ["FileWatching", "InteractiveUtils", "Markdown", "PlutoHooks", "Revise", "UUIDs"]
+git-tree-sha1 = "8f5fa7056e6dcfb23ac5211de38e6c03f6367794"
+uuid = "0ff47ea0-7a50-410d-8455-4348d5de0420"
+version = "0.1.6"
+
+[[deps.PlutoTeachingTools]]
+deps = ["Downloads", "HypertextLiteral", "LaTeXStrings", "Latexify", "Markdown", "PlutoLinks", "PlutoUI", "Random"]
+git-tree-sha1 = "5d9ab1a4faf25a62bb9d07ef0003396ac258ef1c"
+uuid = "661c6b06-c737-4d37-b85c-46df65de6f69"
+version = "0.2.15"
+
+[[deps.PlutoUI]]
+deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "FixedPointNumbers", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "MIMEs", "Markdown", "Random", "Reexport", "URIs", "UUIDs"]
+git-tree-sha1 = "ab55ee1510ad2af0ff674dbcced5e94921f867a9"
+uuid = "7f904dfe-b85e-4ff6-b463-dae2292396a8"
+version = "0.7.59"
+
+[[deps.PrecompileTools]]
+deps = ["Preferences"]
+git-tree-sha1 = "5aa36f7049a63a1528fe8f7c3f2113413ffd4e1f"
+uuid = "aea7be01-6a6a-4083-8856-8a6e6704d82a"
+version = "1.2.1"
+
+[[deps.Preferences]]
+deps = ["TOML"]
+git-tree-sha1 = "9306f6085165d270f7e3db02af26a400d580f5c6"
+uuid = "21216c6a-2e73-6563-6e65-726566657250"
+version = "1.4.3"
+
+[[deps.Printf]]
+deps = ["Unicode"]
+uuid = "de0858da-6303-5e67-8744-51eddeeeb8d7"
+
+[[deps.REPL]]
+deps = ["InteractiveUtils", "Markdown", "Sockets", "Unicode"]
+uuid = "3fa0cd96-eef1-5676-8a61-b3b8758bbffb"
+
+[[deps.Random]]
+deps = ["SHA"]
+uuid = "9a3f8284-a2c9-5f02-9a11-845980a1fd5c"
+
+[[deps.Reexport]]
+git-tree-sha1 = "45e428421666073eab6f2da5c9d310d99bb12f9b"
+uuid = "189a3867-3050-52da-a836-e630ba90ab69"
+version = "1.2.2"
+
+[[deps.Requires]]
+deps = ["UUIDs"]
+git-tree-sha1 = "838a3a4188e2ded87a4f9f184b4b0d78a1e91cb7"
+uuid = "ae029012-a4dd-5104-9daa-d747884805df"
+version = "1.3.0"
+
+[[deps.Revise]]
+deps = ["CodeTracking", "Distributed", "FileWatching", "JuliaInterpreter", "LibGit2", "LoweredCodeUtils", "OrderedCollections", "REPL", "Requires", "UUIDs", "Unicode"]
+git-tree-sha1 = "7b7850bb94f75762d567834d7e9802fc22d62f9c"
+uuid = "295af30f-e4ad-537b-8983-00126c2a3abe"
+version = "3.5.18"
+
+[[deps.SHA]]
+uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
+version = "0.7.0"
+
+[[deps.Serialization]]
+uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
+
+[[deps.Sockets]]
+uuid = "6462fe0b-24de-5631-8697-dd941f90decc"
+
+[[deps.SparseArrays]]
+deps = ["Libdl", "LinearAlgebra", "Random", "Serialization", "SuiteSparse_jll"]
+uuid = "2f01184e-e22b-5df5-ae63-d93ebab69eaf"
+version = "1.10.0"
+
+[[deps.Statistics]]
+deps = ["LinearAlgebra", "SparseArrays"]
+uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
+version = "1.10.0"
+
+[[deps.SuiteSparse_jll]]
+deps = ["Artifacts", "Libdl", "libblastrampoline_jll"]
+uuid = "bea87d4a-7f5b-5778-9afe-8cc45184846c"
+version = "7.2.1+1"
+
+[[deps.TOML]]
+deps = ["Dates"]
+uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
+version = "1.0.3"
+
+[[deps.Tar]]
+deps = ["ArgTools", "SHA"]
+uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
+version = "1.10.0"
+
+[[deps.Test]]
+deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
+uuid = "8dfed614-e22c-5e08-85e1-65c5234f0b40"
+
+[[deps.Tricks]]
+git-tree-sha1 = "eae1bb484cd63b36999ee58be2de6c178105112f"
+uuid = "410a4b4d-49e4-4fbc-ab6d-cb71b17b3775"
+version = "0.1.8"
+
+[[deps.URIs]]
+git-tree-sha1 = "67db6cc7b3821e19ebe75791a9dd19c9b1188f2b"
+uuid = "5c2747f8-b7ea-4ff2-ba2e-563bfd36b1d4"
+version = "1.5.1"
+
+[[deps.UUIDs]]
+deps = ["Random", "SHA"]
+uuid = "cf7118a7-6976-5b1a-9a39-7adc72f591a4"
+
+[[deps.Unicode]]
+uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
+
+[[deps.Zlib_jll]]
+deps = ["Libdl"]
+uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
+version = "1.2.13+1"
+
+[[deps.libblastrampoline_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
+version = "5.8.0+1"
+
+[[deps.nghttp2_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
+version = "1.52.0+1"
+
+[[deps.p7zip_jll]]
+deps = ["Artifacts", "Libdl"]
+uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
+version = "17.4.0+2"
+"""
+
+# ╔═╡ Cell order:
+# ╟─242b68cf-2ca1-4901-9049-07f23c2eabdd
+# ╟─5bdbfc84-62d5-11ef-2bdc-e7186887eee4
+# ╟─49e06755-2873-4db0-9326-83f07252bf7a
+# ╟─c4d5efa7-0dc2-4a54-ac07-b71392d8360f
+# ╟─c9571b9f-8fd7-4145-a517-f562077b5292
+# ╟─8befdede-ffe8-47c4-8676-3a8b1555c795
+# ╟─7fcc2f71-2a60-4e46-a622-5d165e5a1732
+# ╟─b0fb6dad-ad4a-4b66-957f-d2cee7bbaaff
+# ╟─568c35b6-edb4-48dd-b808-08ead8a70127
+# ╟─9c286167-2bf2-4685-ac63-bb0328a8f0cf
+# ╟─fb5cf0cf-edc1-4bda-bd35-9d45ea1bf746
+# ╟─153c5459-62db-46df-8bc2-d258f65adaf6
+# ╟─2d66a73c-8535-4023-a6a7-0d96cc2e78ec
+# ╟─e3d712e2-cd3d-41fe-90c5-bbdc32cc7e28
+# ╟─90d514a7-47e3-4c63-9cde-b73fedd44571
+# ╟─b28c4c40-c155-4607-b3bd-da1392344727
+# ╟─5ff94e1f-312c-49ea-b58c-bd85a6cdf69b
+# ╟─9059bd6e-3e74-4afd-8817-3511862dabe1
+# ╟─1d0ee8b4-ae47-45d3-9d15-f7c5d4504682
+# ╟─edb0f620-a999-42ef-8e2a-9a5ffe185cc2
+# ╟─bd911a20-bae9-455c-a493-2295872185ae
+# ╟─01433609-9d68-4b04-a1e4-391d7c367d87
+# ╟─5595aa1a-c807-4b8a-a64e-f7a81745a2f0
+# ╟─fb0878b4-543f-488d-b62d-3d349904d301
+# ╟─f644526f-f62a-4a79-bced-ecbbd608205f
+# ╟─6b59b1fe-06fd-496f-b2d7-2552866f26ad
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
